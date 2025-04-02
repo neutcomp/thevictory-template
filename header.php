27e42a220caeba9976a55c2d30c00375c444510a
Bjorn van der Neut <bvanderneut@independer.nl>
2025-04-02 15:55:12 +0200

    Update header.php to include dynamic site description and improve title rendering

--- header.php
+++ header.php
@@ -3,7 +3,8 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><!-- wp:site-title /--></title>
+    <title><?php echo get_bloginfo('name'); ?></title>
+		<meta name="description" content="<?php echo get_bloginfo('description'); ?>">
     <link rel="stylesheet" href="<?php echo get_theme_file_uri('style.css'); ?>" />
   </head>
   <body>
