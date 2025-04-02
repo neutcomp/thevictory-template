95608d36d2615064e32f0adff07c4f181775a708
Bjorn van der Neut <bvanderneut@independer.nl>
2025-04-02 15:13:33 +0200

    Update GitHub Actions workflow to ignore additional files and add @biomejs/biome dependency

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         <a href="/"
           ><img
             class="logo"
-            src="./img/the-victory-logo.webp"
+            src="img/the-victory-logo.webp"
             alt="The Victory logo"
         /></a>
 
