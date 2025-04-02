f18b1b89500311c11a02ebc8ed376117c92f206c
Bjorn van der Neut <bvanderneut@independer.nl>
2025-04-02 15:40:54 +0200

    Update header.php to use dynamic site title with WordPress block

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Tafeltennis Vereniging The Victory uit Weesp</title>
+    <title><!-- wp:site-title /--></title>
     <link rel="stylesheet" href="<?php echo get_theme_file_uri('style.css'); ?>" />
   </head>
   <body>
