cc77b437ef8a2fda8936fb8ef0a7c9ffeccb9fec
Bjorn van der Neut <bvanderneut@independer.nl>
2025-04-02 15:46:21 +0200

    Add page.php and singular.php templates for dynamic content display

---
+++ page.php
@@ -0,0 +1,5 @@
+<div class="container">
+	// Page content
+	<h1><?php the_title(); ?></h1>
+	<p><?php the_content(); ?></p>
+</div>
---
+++ singular.php
@@ -0,0 +1,5 @@
+<div class="container">
+	// Singular content
+	<h1><?php the_title(); ?></h1>
+	<p><?php the_content(); ?></p>
+</div>
