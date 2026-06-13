1359ac75233c85318fae03db491dde7c0c08d5f4
Bjorn van der Neut <bjornvdneut+github@gmail.com>
2026-06-13 12:19:36 +0200

    Remove page.php

--- bjorn-sing-ular.php
+++
@@ -1,4 +0,0 @@
-<div class="container">
-	<h1><?php the_title(); ?></h1>
-	<p><?php the_content(); ?></p>
-</div>
--- page.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-	// Page content
-	<h1><?php the_title(); ?></h1>
-	<p><?php the_content(); ?></p>
-</div>
